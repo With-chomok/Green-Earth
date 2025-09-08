8543dcbff9724b54ce922e697cd2d8fb8323cdb5
Chomok Das <183892208+With-chomok@users.noreply.github.com>
2025-09-08 16:26:48 +0600

    cart container  handle and dislay my cart and some changes added

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const catagoryTitleName = document.getElementById("catagoryTitle");
 const catagorycardDetails = document.getElementById("cardDetails");
+const cartContainerDetails = document.getElementById("cartContainer");
+const cartContainertotal = document.getElementById("cartContainerCount")
+let carts = [];
 const loadCatagory = () => {
   fetch("https://openapi.programming-hero.com/api/categories")
     .then((res) => res.json())
@@ -21,8 +24,6 @@ const displayCatagoryTitle = (catagory) => {
 
   // Category Click → Tree Data On clicking a category: load trees of that category.
   catagoryTitleName.addEventListener("click", (e) => {
-    // console.log(e.target.id);
-
     if (e.target.localName === "p") {
       [...catagoryTitleName.querySelectorAll("p")].forEach((el) => {
         el.classList.remove("bg-[#15803D]", "text-white");
@@ -40,7 +41,7 @@ const loadCatagoryDetails = (targetId) => {
   fetch(`https://openapi.programming-hero.com/api/category/${targetId}`)
     .then((res) => res.json())
     .then((data) => {
-        const dataPlants = data.plants;
+      const dataPlants = data.plants;
       displayLoadCatagoriesDetails(dataPlants);
     })
     .catch((err) => {
@@ -49,17 +50,29 @@ const loadCatagoryDetails = (targetId) => {
 };
 
 
+const defaultLoadCatagory = (plants) => {
+  fetch("https://openapi.programming-hero.com/api/plants")
+   .then((res) => res.json())
+    .then((data) => {
+      const dataPlants = data.plants;
+      displayLoadCatagoriesDetails(dataPlants);
+    })
+}
+
 const displayLoadCatagoriesDetails = (details) => {
-    details.forEach(detail => {
-        catagorycardDetails.innerHTML += `
+  catagorycardDetails.innerHTML = "";
+  details.forEach((detail) => {
+    // console.log(detail);
+
+    catagorycardDetails.innerHTML += `
         <div class="card bg-base-100 shadow-sm p-4 h-full flex flex-col">
-          <div class="space-y-3 h-full flex flex-col">
+          <div id="${detail.id}" class="space-y-3 h-full flex flex-col">
             <figure class="bg-gray-300 rounded-xl">
               <img src=${detail.image} alt="" class="w-full h-40 object-cover" />
             </figure>
 
-            <h2 class="card-title">${detail.name}</h2>
-            <p class="flex-grow">
+            <h2 onclick="showModal()" class="card-title">${detail.name}</h2>
+            <p class="flex-grow line-clamp-3">
               ${detail.description}
             </p>
             <div class="flex justify-between items-center">
@@ -67,10 +80,10 @@ const displayLoadCatagoriesDetails = (details) => {
                 class="bg-[#DCFCE7] text-[#15803D] px-3 py-1 rounded-[400px] font-medium">
                 ${detail.category}
               </h1>
-              <h1>${detail.price}</h1>
+              <h1>৳<span>${detail.price}</span></h1>
             </div>
 
-            <button
+            <button id = "catagoryCardBtn"
               class="btn bg-[#15803D] text-sm text-white rounded-full w-full shadow-none border-none">
               Add to Cart
             </button>
@@ -78,8 +91,118 @@ const displayLoadCatagoriesDetails = (details) => {
         </div>
         
         `;
-    })
-    
+        
+  });
 };
 
+
+
+// const countCartContainer = () => {
+
+// }
+catagorycardDetails.addEventListener("click", (e) => {
+  // console.log(e.target.innerText);
+  if (e.target.innerText === "Add to Cart") {
+    handleCartContainer(e);
+  }
+});
+
+const handleCartContainer = (e) => {
+  // console.log(e.target);
+  
+  const cartTitle = e.target.parentNode.children[1].innerText;
+  const cartamount =
+  e.target.parentNode.childNodes[7].children[1].children[0].innerText;
+  const id = e.target.parentNode.id;
+  // console.log(id);
+  
+  const parseCartamount = parseInt(cartamount);
+  // console.log(parseCartamount);
+  
+  
+  
+  carts.push({
+    id: id,
+    cartTitle: cartTitle,
+    parseCartamount: parseCartamount,
+  });
+  // console.log(carts);
+  
+  
+  displayCartContainer(carts);
+};
+
+const displayCartContainer = (carts) => {
+  // console.log(carts);
+  cartContainer.innerHTML = "";
+  
+  carts.forEach((cartList) => {
+    // console.log(cartList.id);
+
+    // console.log(cartList);
+
+    cartContainerDetails.innerHTML += `
+    
+    <div class = "flex justify-between items-center bg-[#F0FDF4] rounded-md gap-3 px-2 py-3 shadow-md">
+    <div class="space-y-2 ">
+    <h1 class="font-semibold text-[15px] font-sans block">${cartList.cartTitle}</h1>
+    <p class="text-[#5C5C5C] text-sm">৳${cartList.parseCartamount} x 1</p>
+    </div>
+    
+    
+    <div>
+    <p onclick="handleDelete('${cartList.id}')" class="text-2xl font-sans text-[#8C8C8C] cursor-pointer">x</p>
+    </div>
+    
+    </div>
+    
+    
+    
+    `;
+    cartContainertotal.innerHTML = "";
+    cartContainertotal.innerHTML += `
+    <div  class="flex justify-between items-center">
+    <h1 class="font-medium text-base text-[#1F2937]">Total:</h1>
+    <p class="font-medium text-base text-[#1F2937]">৳<span>${cartList.parseCartamount}</p>
+    </div>
+        `;
+
+        
+        
+  });
+};
+
+const handleDelete = (deleteId) => {
+  const filtered = carts.filter((cartList) => cartList.id !== deleteId);
+  carts = filtered;
+  displayCartContainer(carts);
+};
+
+// const showModal =() => {
+//   fetch("https://openapi.programming-hero.com/api/plant/1")
+//   .then(res => res.json())
+//   .then(data => {
+
+
+//     data.innerHTML += `
+    
+    
+//     // <dialog id="modal" class="modal modal-bottom sm:modal-middle">
+//     //         <div class="modal-box">
+//     //           <h3 class="text-lg font-bold">Hello!</h3>
+//     //           <p class="py-4">Press ESC key or click the button below to close</p>
+//     //           <div class="modal-action">
+//     //             <form method="dialog">
+//     //               {/* if there is a button in form, it will close the modal  */}
+//     //               <button class="btn">Close</button>
+//     //             </form>
+//     //           </div>
+//     //         </div>
+//     //       </dialog>
+//     `
+//   })
+//     }
 loadCatagory();
+defaultLoadCatagory()
+
+
